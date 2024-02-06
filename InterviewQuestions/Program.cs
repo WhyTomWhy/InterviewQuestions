d65f2d8c50ee67a91d051d40c6e789d7cc2d58ef
WhyTomWhy <travis.rous@hotmail.com>
2024-02-05 19:31:33 -0700

    Set initial state for questions

--- InterviewQuestions/Program.cs
+++ InterviewQuestions/Program.cs
@@ -4,6 +4,6 @@ using InterviewQuestions;
 Console.WriteLine("Hello, World!");
 
 
-//Question1.PrintFiles();
+Question1.PrintFiles();
 //Question2.CreateAndReadData();
-Question3.BuildList(1);
+//Question3.BuildList(1);
